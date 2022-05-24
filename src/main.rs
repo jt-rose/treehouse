5391cc9b52b9ed2618363711d96d405f95bd2cd6
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-05-23 22:38:57 -0400

    add visitor struct and pattern matching with iterator - find

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@
 
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn get_name() -> String {
     // ask user for their name
     let mut your_name = String::new();
@@ -28,18 +46,15 @@ fn main() {
 
     let name = get_name();
 
-    let visitor_list = ["frodo", "gandalf", "aragon"];
-    let mut allow_in = false;
+    let visitor_list = [
+        Visitor::new("Frodo", "safe travels, Frodo!"),
+        Visitor::new("Gandalf", "Well met, old wizard"),
+        Visitor:: new("Aragon", "Hail to the future king!")
+    ];
 
-    for visitor in &visitor_list {
-        if &name == visitor {
-            allow_in = true;
-        }
-    }
-
-    if allow_in {
-        println!("Welcome to the club, {}!", capitalize(&name));
-    } else {
-        println!("Scram!");
+    let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+    match known_visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("You aren't on the list!"),
     }
 }
